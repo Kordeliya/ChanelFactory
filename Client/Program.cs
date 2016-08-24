582ad566b9d984eca81fbf1725437c464a172a5e
Kordeliya <miss-neismit1991@yandex.ru>
2016-08-24 16:48:37 +0400

    Add Projects

---
+++ Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using InterfacesLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+            using (ChannelFactory<IService> channelFactory = new ChannelFactory<IService>(binding))
+            {
+                IService service = channelFactory.CreateChannel(new EndpointAddress("http://localhost:54799/Service1.svc"));
+                try
+                {
+                    var result = service.GetBook("Горе от ума");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+    }
+}
---
+++ InterfacesLibrary/IService.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesLibrary
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        Book GetBook(string value);
+
+    }
+}
